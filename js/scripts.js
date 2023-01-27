00e379dd5674030de3db04db3354f42b381e128f
Tyler Adams <skipmarcel@gmail.com>
2023-01-27 15:38:41 -0800

    add working if and else if statements

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,18 @@
 //   document.querySelector("form").onsubmit = function (event) {
 //     event.preventDefault();
 //   };
-// function jsRemoveScreen() {
-//   let jsId = document.getElementById("js-id");
-//   jsId.classList.remove("javaScript-screen");
-// }
-// function pythonRemoveScreen() {
-//   let pythonId = document.getElementById("python-id");
-//   pythonId.classList.remove("python-screen");
-// }
-// function reactRemoveScreen() {
-//   let reactId = document.getElementById("react-id");
-//   reactId.classList.remove("react-screen");
-// }
+function jsRemoveScreen() {
+  let jsId = document.getElementById("js-id");
+  jsId.classList.remove("javaScript-screen");
+}
+function pythonRemoveScreen() {
+  let pythonId = document.getElementById("python-id");
+  pythonId.classList.remove("python-screen");
+}
+function reactRemoveScreen() {
+  let reactId = document.getElementById("react-id");
+  reactId.classList.remove("react-screen");
+}
 function rubyRemoveScreen() {
   let rubyId = document.getElementById("ruby-id");
   rubyId.classList.remove("ruby-screen");
@@ -21,15 +21,45 @@ function rubyRemoveScreen() {
 
 document.querySelector("form").onsubmit = function (event) {
   let vw = document.getElementById("vw");
+  let mnms = document.getElementById("mnms");
+  let potatoChips = document.getElementById("potatoChips");
+  let dolphins = document.getElementById("dolphins");
+  let boat = document.getElementById("boat");
+  let nova = document.getElementById("nova");
   // console.log(vw.checked);
-  if (vw.checked == true) {
+  if (mnms.checked == true && boat.checked == true && vw.checked == true) {
     rubyRemoveScreen();
+  } else if (
+    mnms.checked == true &&
+    dolphins.checked == true &&
+    nova.checked == true
+  ) {
+    pythonRemoveScreen();
+  } else if (
+    potatoChips.checked == true &&
+    boat.checked == true &&
+    vw.checked == true
+  ) {
+    reactRemoveScreen();
+  } else if (
+    potatoChips.checked == true &&
+    dolphins.checked == true &&
+    nova.checked == true
+  ) {
+    jsRemoveScreen();
+  } else if (
+    vw.checked == true &&
+    dolphins.checked == true &&
+    mnms.checked == true
+  ) {
+    rubyRemoveScreen();
+  } else if (
+    nova.checked == true &&
+    boat.checked == true &&
+    potatoChips.checked == true
+  ) {
+    pythonRemoveScreen();
   }
   event.preventDefault();
 };
 // };
-
-//   function rubyRemoveScreen() {
-//     let rubyId = document.getElementById("ruby-id");
-//     rubyId.classList.remove("ruby-screen");
-//   }
