2d596c616d0eba96fba2010930cee7ca6346e7aa
Tyler Adams <skipmarcel@gmail.com>
2023-01-27 10:26:42 -0800

    try out javaScript code for SubmitEvent, not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,30 @@
-window.onload = function () {
-  document.querySelector("form").onsubmit = function (event) {
-    event.preventDefault();
-  };
-  function jsRemoveScreen() {
-    let jsId = document.getElementById("js-id");
-    jsId.classList.remove("javaScript-screen");
-  }
-  function pythonRemoveScreen() {
-    let pythonId = document.getElementById("python-id");
-    pythonId.classList.remove("python-screen");
-  }
-  function reactRemoveScreen() {
-    let reactId = document.getElementById("react-id");
-    pythonId.classList.remove("react-screen");
-  }
-  function rubyRemoveScreen() {
-    let rubyId = document.getElementById("ruby-id");
-    pythonId.classList.remove("ruby-screen");
-  }
+// window.onload = function () {
+//   document.querySelector("form").onsubmit = function (event) {
+//     event.preventDefault();
+//   };
+function jsRemoveScreen() {
+  let jsId = document.getElementById("js-id");
+  jsId.classList.remove("javaScript-screen");
+}
+function pythonRemoveScreen() {
+  let pythonId = document.getElementById("python-id");
+  pythonId.classList.remove("python-screen");
+}
+function reactRemoveScreen() {
+  let reactId = document.getElementById("react-id");
+  reactId.classList.remove("react-screen");
+}
+function rubyRemoveScreen() {
+  let rubyId = document.getElementById("ruby-id");
+  rubyId.classList.remove("ruby-screen");
+}
+
+document.getElementById("form-submit").onsubmit = function () {
+  rubyRemoveScreen();
 };
+// };
+
+//   function rubyRemoveScreen() {
+//     let rubyId = document.getElementById("ruby-id");
+//     rubyId.classList.remove("ruby-screen");
+//   }
