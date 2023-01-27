56cf5e14029aeadcf468a9748951b8778d8ea09a
Tyler Adams <skipmarcel@gmail.com>
2023-01-27 11:42:39 -0800

    try new js functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,27 @@
 //   document.querySelector("form").onsubmit = function (event) {
 //     event.preventDefault();
 //   };
-function jsRemoveScreen() {
-  let jsId = document.getElementById("js-id");
-  jsId.classList.remove("javaScript-screen");
-}
-function pythonRemoveScreen() {
-  let pythonId = document.getElementById("python-id");
-  pythonId.classList.remove("python-screen");
-}
-function reactRemoveScreen() {
-  let reactId = document.getElementById("react-id");
-  reactId.classList.remove("react-screen");
-}
+// function jsRemoveScreen() {
+//   let jsId = document.getElementById("js-id");
+//   jsId.classList.remove("javaScript-screen");
+// }
+// function pythonRemoveScreen() {
+//   let pythonId = document.getElementById("python-id");
+//   pythonId.classList.remove("python-screen");
+// }
+// function reactRemoveScreen() {
+//   let reactId = document.getElementById("react-id");
+//   reactId.classList.remove("react-screen");
+// }
 function rubyRemoveScreen() {
   let rubyId = document.getElementById("ruby-id");
   rubyId.classList.remove("ruby-screen");
 }
 
-document.getElementById("form-submit").onsubmit = function () {
+document.querySelector("form").onsubmit = function (event) {
+  let vw = document.getElementById("vw");
   rubyRemoveScreen();
+  event.preventDefault();
 };
 // };
 
