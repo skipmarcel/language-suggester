4a19b0412129b00b60e85ecfc7aad74fb5699098
Tyler Adams <skipmarcel@gmail.com>
2023-01-26 23:32:49 -0800

    add javaScript functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 window.onload = function () {
   document.querySelector("form").onsubmit = function (event) {
     event.preventDefault();
-    hideResultsAndError();
   };
+  function jsRemoveScreen() {
+    let jsId = document.getElementById("js-id");
+    jsId.classList.remove("javaScript-screen");
+  }
+  function pythonRemoveScreen() {
+    let pythonId = document.getElementById("python-id");
+    pythonId.classList.remove("python-screen");
+  }
+  function reactRemoveScreen() {
+    let reactId = document.getElementById("react-id");
+    pythonId.classList.remove("react-screen");
+  }
+  function rubyRemoveScreen() {
+    let rubyId = document.getElementById("ruby-id");
+    pythonId.classList.remove("ruby-screen");
+  }
 };
