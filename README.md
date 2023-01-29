2be986ef24d61a74e384445be7fede0247c4ef45
Tyler Adams <skipmarcel@gmail.com>
2023-01-28 19:56:39 -0800

    add README file and un-comment window onload

---
+++ README.md
@@ -0,0 +1,32 @@
+# _Programing Language Suggester_
+
+#### By _**Tyler Adams**_
+
+#### \_\_
+
+#### https://github.com/skipmarcel/language-suggester.git
+
+## Technologies Used
+
+- _HTML_
+- _CSS_
+- _javaScript_
+
+## Description
+
+_This is a simple guide to help someone who is considering learning how to code._
+
+## Setup/Installation Requirements
+
+- _This page runs with a standard index.html, styles.css and scripts.js file_
+- _There is a googlepis link in the HTML head for a font reference_
+
+## Known Bugs
+
+- _Certain combinations of inputs will fire the error function when its not neccissary. If else statements are suspect_
+
+## License
+
+_If there are any isues you find, please contact me via email: skipmarcel@gmail.com_
+
+Copyright (c) _01/28/2023_ _Tyler Adams_
--- js/scripts.js
+++ js/scripts.js
@@ -1,83 +1,80 @@
-// window.onload = function () {
-//   document.querySelector("form").onsubmit = function (event) {
-//     event.preventDefault();
-//   };
-function jsRemoveScreen() {
-  let jsId = document.getElementById("js-id");
-  jsId.classList.remove("javaScript-screen");
-}
-function pythonRemoveScreen() {
-  let pythonId = document.getElementById("python-id");
-  pythonId.classList.remove("python-screen");
-}
-function reactRemoveScreen() {
-  let reactId = document.getElementById("react-id");
-  reactId.classList.remove("react-screen");
-}
-function rubyRemoveScreen() {
-  let rubyId = document.getElementById("ruby-id");
-  rubyId.classList.remove("ruby-screen");
-}
-
-document.querySelector("form").onsubmit = function (event) {
-  let vw = document.getElementById("vw");
-  let mnms = document.getElementById("mnms");
-  let potatoChips = document.getElementById("potatoChips");
-  let dolphins = document.getElementById("dolphins");
-  let boat = document.getElementById("boat");
-  let nova = document.getElementById("nova");
-  let radioInput = document.querySelector("input");
-  function restForm() {
-    document.getElementById("");
+window.onload = function () {
+  //   document.querySelector("form").onsubmit = function (event) {
+  //     event.preventDefault();
+  //   };
+  function jsRemoveScreen() {
+    let jsId = document.getElementById("js-id");
+    jsId.classList.remove("javaScript-screen");
+  }
+  function pythonRemoveScreen() {
+    let pythonId = document.getElementById("python-id");
+    pythonId.classList.remove("python-screen");
+  }
+  function reactRemoveScreen() {
+    let reactId = document.getElementById("react-id");
+    reactId.classList.remove("react-screen");
+  }
+  function rubyRemoveScreen() {
+    let rubyId = document.getElementById("ruby-id");
+    rubyId.classList.remove("ruby-screen");
   }
 
-  if (mnms.checked == true && boat.checked == true && vw.checked == true) {
-    rubyRemoveScreen();
-  } else if (
-    mnms.checked == true &&
-    dolphins.checked == true &&
-    nova.checked == true
-  ) {
-    pythonRemoveScreen();
-  } else if (
-    potatoChips.checked == true &&
-    boat.checked == true &&
-    vw.checked == true
-  ) {
-    reactRemoveScreen();
-  } else if (
-    potatoChips.checked == true &&
-    dolphins.checked == true &&
-    nova.checked == true
-  ) {
-    jsRemoveScreen();
-  } else if (
-    vw.checked == true &&
-    dolphins.checked == true &&
-    mnms.checked == true
-  ) {
-    rubyRemoveScreen();
-  } else if (
-    nova.checked == true &&
-    boat.checked == true &&
-    potatoChips.checked == true
-  ) {
-    pythonRemoveScreen();
-  } else if (
-    vw.checked == true &&
-    dolphins.checked == true &&
-    potatoChips.checked == true
-  ) {
-    reactRemoveScreen();
-  } else if (radioInput.checked == 1 || 2) {
-    document.getElementById("errorMessage").innerHTML =
-      "ANSWER ALL THREE QUESTIONS";
-  }
+  document.querySelector("form").onsubmit = function (event) {
+    let vw = document.getElementById("vw");
+    let mnms = document.getElementById("mnms");
+    let potatoChips = document.getElementById("potatoChips");
+    let dolphins = document.getElementById("dolphins");
+    let boat = document.getElementById("boat");
+    let nova = document.getElementById("nova");
+    let radioInput = document.querySelector("input");
+    function restForm() {
+      document.getElementById("");
+    }
 
-  // if (mnms.checked == false || potatoChips.checked == false) {
-  //   document.getElementById("errorMessage").innerHTML = "error";
-  // }
-  document.getElementById("form-submit").reset();
-  event.preventDefault();
+    if (mnms.checked == true && boat.checked == true && vw.checked == true) {
+      rubyRemoveScreen();
+    } else if (
+      mnms.checked == true &&
+      dolphins.checked == true &&
+      nova.checked == true
+    ) {
+      pythonRemoveScreen();
+    } else if (
+      potatoChips.checked == true &&
+      boat.checked == true &&
+      vw.checked == true
+    ) {
+      reactRemoveScreen();
+    } else if (
+      potatoChips.checked == true &&
+      dolphins.checked == true &&
+      nova.checked == true
+    ) {
+      jsRemoveScreen();
+    } else if (
+      vw.checked == true &&
+      dolphins.checked == true &&
+      mnms.checked == true
+    ) {
+      rubyRemoveScreen();
+    } else if (
+      nova.checked == true &&
+      boat.checked == true &&
+      potatoChips.checked == true
+    ) {
+      pythonRemoveScreen();
+    } else if (
+      vw.checked == true &&
+      dolphins.checked == true &&
+      potatoChips.checked == true
+    ) {
+      reactRemoveScreen();
+    } else if (radioInput.checked == 1 || 2) {
+      document.getElementById("errorMessage").innerHTML =
+        "ANSWER ALL THREE QUESTIONS";
+    }
+
+    document.getElementById("form-submit").reset();
+    event.preventDefault();
+  };
 };
-// };
