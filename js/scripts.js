071d114aa401838d11fc8602b6408c609450d2e4
Tyler Adams <skipmarcel@gmail.com>
2023-02-01 06:48:41 -0800

    add functions to hide previous results

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 window.onload = function () {
-  //   document.querySelector("form").onsubmit = function (event) {
-  //     event.preventDefault();
-  //   };
+  function jsReplaceScreen() {
+    let jsId = document.getElementById("js-id");
+    jsId.classList.add("javaScript-screen");
+  }
+  function pythonReplaceScreen() {
+    let pythonId = document.getElementById("python-id");
+    pythonId.classList.add("python-screen");
+  }
+  function reactReplaceScreen() {
+    let reactId = document.getElementById("react-id");
+    reactId.classList.add("react-screen");
+  }
+  function rubyReplaceScreen() {
+    let rubyId = document.getElementById("ruby-id");
+    rubyId.classList.add("ruby-screen");
+  }
+
   function jsRemoveScreen() {
     let jsId = document.getElementById("js-id");
     jsId.classList.remove("javaScript-screen");
@@ -27,48 +41,70 @@ window.onload = function () {
     let boat = document.getElementById("boat");
     let nova = document.getElementById("nova");
     let radioInput = document.querySelector("input");
+    //this is a function that resets radio buttons only:
     function restForm() {
       document.getElementById("");
     }
 
     if (mnms.checked == true && boat.checked == true && vw.checked == true) {
       rubyRemoveScreen();
+      pythonReplaceScreen();
+      jsReplaceScreen();
+      reactReplaceScreen();
     } else if (
       mnms.checked == true &&
       dolphins.checked == true &&
       nova.checked == true
     ) {
       pythonRemoveScreen();
+      jsReplaceScreen();
+      reactReplaceScreen();
+      rubyReplaceScreen();
     } else if (
       potatoChips.checked == true &&
       boat.checked == true &&
       vw.checked == true
     ) {
       reactRemoveScreen();
+      rubyReplaceScreen();
+      jsReplaceScreen();
+      pythonReplaceScreen();
     } else if (
       potatoChips.checked == true &&
       dolphins.checked == true &&
       nova.checked == true
     ) {
       jsRemoveScreen();
+      rubyReplaceScreen();
+      reactReplaceScreen();
+      pythonReplaceScreen();
     } else if (
       vw.checked == true &&
       dolphins.checked == true &&
       mnms.checked == true
     ) {
       rubyRemoveScreen();
+      pythonReplaceScreen();
+      jsReplaceScreen();
+      reactReplaceScreen();
     } else if (
       nova.checked == true &&
       boat.checked == true &&
       potatoChips.checked == true
     ) {
       pythonRemoveScreen();
+      jsReplaceScreen();
+      reactReplaceScreen();
+      rubyReplaceScreen();
     } else if (
       vw.checked == true &&
       dolphins.checked == true &&
       potatoChips.checked == true
     ) {
       reactRemoveScreen();
+      rubyReplaceScreen();
+      jsReplaceScreen();
+      pythonReplaceScreen();
     } else if (radioInput.checked == 1 || 2) {
       document.getElementById("errorMessage").innerHTML =
         "ANSWER ALL THREE QUESTIONS";
